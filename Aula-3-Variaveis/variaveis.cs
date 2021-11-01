227bae0f73fad004ca8f7bcd758129c7a39c0be2
Natali Soares <natalisoares90@gmail.com>
2021-11-01 12:20:36 -0300

    Multiplas Variaveis

---
+++ Aula-3-Variaveis/variaveis.cs
@@ -0,0 +1,28 @@
+using System;
+
+class variaveis {
+    static void Main () {
+        // tipos de variaveis 
+
+        int num1, num2, res;
+
+        num1 = 10;
+        num2 = 2;
+        res = num1 * num2;
+
+       Console.WriteLine(" A multiplicação de " + num1 + " mais " + num2 + " é igual a: " + res);
+
+
+
+
+        int numero = 0;
+        char letra = 'c';
+        float valor=5.3f;
+        string nome = "Bruno";
+
+        var aux = nome;
+
+        Console.WriteLine("Valor da variável:" + aux + "03") ;
+
+    }
+}
